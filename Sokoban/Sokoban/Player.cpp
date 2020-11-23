f3e4f2488e7e3a74975128948d74e471d3eb5a36
HavensurgeSW <matias@karplus.com.ar>
2020-11-23 16:21:05 -0300

    Box collisions done. Starting movement bug?

--- Sokoban/Sokoban/Player.cpp
+++ Sokoban/Sokoban/Player.cpp
@@ -19,7 +19,6 @@ namespace SB {
 
 	void movePlayer() {
 		bool canMove = true;
-		bool canMoveBlock = true;
 		if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT)) {
 #if DEBUG
 			cout << "Enter Key D" << endl;
@@ -34,6 +33,16 @@ namespace SB {
 					block[i].posY++;
 					
 				}
+				for (int j = 0; j < 6; j++) {
+					if (block[i].posX == block[j].posX&&block[i].posY == block[j].posY&&i != j) {
+						block[i].rec.x = tile[player.posX][player.posY + 1].rec.x;
+						block[i].rec.y = tile[player.posX][player.posY + 1].rec.y;
+						block[i].posY--;
+						canMove = false;
+					}
+				}
+
+
 				if ((player.posY + 1 == block[i].posY&&player.posX == block[i].posX && tile[player.posX][player.posY + 2].id != 6) && canMove&&block[i].active
 					|| canMove&&tile[player.posX][player.posY + 1].id != 6) {
 					canMove = false;
@@ -61,6 +70,15 @@ namespace SB {
 					block[i].rec.x = tile[player.posX][player.posY - 2].rec.x;
 					block[i].rec.y = tile[player.posX][player.posY - 2].rec.y;
 					block[i].posY--;
+				}
+
+				for (int j = 0; j < 6; j++) {
+					if (block[i].posX == block[j].posX&&block[i].posY == block[j].posY&&i != j) {
+						block[i].rec.x = tile[player.posX][player.posY - 1].rec.x;
+						block[i].rec.y = tile[player.posX][player.posY - 1].rec.y;
+						block[i].posY++;
+						canMove = false;
+					}
 				}
 				if ((player.posY - 1 == block[i].posY&&player.posX == block[i].posX && tile[player.posX][player.posY - 2].id != 6) && canMove&&block[i].active || tile[player.posX][player.posY - 1].id != 6) {
 					canMove = false;
@@ -92,9 +110,16 @@ namespace SB {
 						block[i].rec.x = tile[player.posX - 2][player.posY].rec.x;
 						block[i].rec.y = tile[player.posX - 2][player.posY].rec.y;
 						block[i].posX--;
-						
-						
 					}
+					for (int j = 0; j < 6; j++) {
+						if (block[i].posX == block[j].posX&&block[i].posY == block[j].posY&&i != j) {
+							block[i].rec.x = tile[player.posX - 1][player.posY].rec.x;
+							block[i].rec.y = tile[player.posX - 1][player.posY].rec.y;
+							block[i].posX++;
+							canMove = false;
+						}
+					}
+
 					if ((player.posX - 1 == block[i].posX&&player.posY == block[i].posY && tile[player.posX - 2][player.posY].id != 6) && canMove&&block[i].active || tile[player.posX - 1][player.posY].id != 6) {
 						canMove = false;
 					
@@ -119,24 +144,22 @@ namespace SB {
 
 				for (int i = 0; i < 6; i++) {
 					if (block[i].posX == player.posX + 1 && block[i].posY == player.posY && tile[player.posX + 2][player.posY].id == 6) {
-
-						
-						for (int j = 0; j < 6; j++){
-							if (block[i].posX + 2 == block[j].posX){
-								canMoveBlock = false;
-							}
-						}
-						if (!canMoveBlock){
 							block[i].rec.x = tile[player.posX + 2][player.posY].rec.x;
 							block[i].rec.y = tile[player.posX + 2][player.posY].rec.y;
 							block[i].posX++;
-							canMoveBlock = true;
+						
+						for (int j = 0; j < 6; j++){
+							if (block[i].posX==block[j].posX&&block[i].posY == block[j].posY&&i!=j){
+								block[i].rec.x = tile[player.posX + 1][player.posY].rec.x;
+								block[i].rec.y = tile[player.posX + 1][player.posY].rec.y;
+								block[i].posX--;
+								canMove = false;
+							}
 						}
+
 					}
 					if ((player.posX + 1 == block[i].posX&&player.posY == block[i].posY && tile[player.posX + 2][player.posY].id != 6) && canMove&&block[i].active || tile[player.posX + 1][player.posY].id != 6) {
 						canMove = false;
-						canMoveBlock = false;
-
 					}
 
 				}
